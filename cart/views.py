d7f99b8537e363141b8a9483b033c76e329c4fb3
unknown <celixscondolo@gmail.com>
2024-01-16 12:05:49 -0500

    fix button add cart

--- cart/views.py
+++ cart/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
-from django.shortcuts import render
-from .models import CartItem
+from django.http import JsonResponse
 from products.models import Product
+from django.shortcuts import render
+from django.contrib import messages
+from .models import CartItem
+import json
 
 
 # Create your views here.
 def cart(request):
     return render(request, 'cart/cart.html')
 
-
 def add_to_cart(request, product_id):
     if request.user.is_authenticated:
         product = get_object_or_404(Product, pk=product_id)
-        quantity = int(request.POST.get('quantity', 1))
+        payload = json.loads(request.body)
+        quantity = payload.get('quantity', 1)
 
         # Crear o actualizar el item del carrito
         cart_item, created = CartItem.objects.get_or_create(
