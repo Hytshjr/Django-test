b6945a7f9d196059fde9c3f70a116534d238ffb8
unknown <celixscondolo@gmail.com>
2024-01-22 17:24:07 -0500

    fix css

--- checkout/views.py
+++ checkout/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def delivery(request):
-    return render(request, 'checkout\delivery.html')
+    return render(request, 'checkout\delivery.html')    
